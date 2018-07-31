09614bbfdf8031a94c3fca2dbc1cec66c72b45eb
Satyakumar BS <bs.satya@gmail.com>
2018-08-01 03:05:58 +0530

    added addShoppingItem page again

--- src/pages/add-shopping-item/add-shopping-item.module.ts
+++ src/pages/add-shopping-item/add-shopping-item.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular/';
-import { AddShoppingItemPage } from './addShoppingItem';
+import { IonicPageModule } from 'ionic-angular';
+import { AddShoppingItemPage } from './add-shopping-item';
 
 @NgModule({
   declarations: [
--- src/pages/add-shopping-item/add-shopping-item.ts
+++ src/pages/add-shopping-item/add-shopping-item.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular/';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
  * Generated class for the AddShoppingItemPage page.
@@ -11,7 +11,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular/';
 @IonicPage()
 @Component({
   selector: 'page-add-shopping-item',
-  templateUrl: 'addShoppingItem.html',
+  templateUrl: 'add-shopping-item.html',
 })
 export class AddShoppingItemPage {
 
