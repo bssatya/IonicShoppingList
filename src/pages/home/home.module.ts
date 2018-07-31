9370960bea27a8e96ce4131650a0228296101a1e
Satyakumar BS <bs.satya@gmail.com>
2018-08-01 00:44:46 +0530

    fixed the iconic serve issue

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { HomePage } from './home';
-import { IonicPageModule } from '../../../node_modules/ionic-angular/umd';
+import { IonicPageModule } from '../../../node_modules/ionic-angular/';
 
 @NgModule ({
   declarations: [HomePage],
